2ce35a4d7eb9b6a80e8443ebfd2be1d58f6e28b8
michelangelo-fi <michelangelo.corsari@edu.unifi.com>
2026-07-02 17:13:39 +0200

    configurazione per deploy su Railway

--- event_management/settings.py
+++ event_management/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+#per railway deployment
+ALLOWED_HOSTS = ['*']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   #per railway deployment
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,5 +123,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# I added this line to specify the custom user model I am using in my project, which is defined in the accounts app as User.
+# questa variabile serve per dire a Django di usare il mio modello User personalizzato invece di quello di default
 AUTH_USER_MODEL = 'accounts.User'
+
+# Configurazione file statici
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
